94db4be26b643afc9fe8d030fa886817bdc96d4a
Matheus Ranzani <ranzani.matheus@gmail.com>
2024-01-03 14:47:28 -0300

    Curso 1 concluído

---
+++ boas-praticas-em-java/src/main/java/br/com/alura/AdopetConsoleApplication.java
@@ -0,0 +1,46 @@
+package br.com.alura;
+
+import java.util.Scanner;
+
+public class AdopetConsoleApplication {
+    public static void main(String[] args) {
+        CommandExecutor executor = new CommandExecutor();
+
+        System.out.println("##### BOAS VINDAS AO SISTEMA ADOPET CONSOLE #####");
+        try {
+            int opcaoEscolhida = 0;
+            while (opcaoEscolhida != 5) {
+                exibirMenu();
+
+                String textoDigitado = new Scanner(System.in).nextLine();
+                opcaoEscolhida = Integer.parseInt(textoDigitado);
+
+                switch (opcaoEscolhida) {
+                    case 1 -> executor.executeCommand(new ListarAbrigoCommand());
+                    case 2 -> executor.executeCommand(new CadastrarAbrigoCommand());
+                    case 3 -> executor.executeCommand(new ListarPetsDoAbrigoCommand());
+                    case 4 -> executor.executeCommand(new ImportarPetsDoAbrigoCommand());
+                    case 5 -> {
+                        System.out.println("Finalizando o programa...");
+                        System.exit(0);
+                    }
+                    default -> {
+                        System.out.println("Opção inválida! Tente novamente...");
+                        opcaoEscolhida = 0;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void exibirMenu() {
+        System.out.println("\nDIGITE O NÚMERO DA OPERAÇÃO DESEJADA:");
+        System.out.println("1 -> Listar abrigos cadastrados");
+        System.out.println("2 -> Cadastrar novo abrigo");
+        System.out.println("3 -> Listar pets do abrigo");
+        System.out.println("4 -> Importar pets do abrigo");
+        System.out.println("5 -> Sair");
+    }
+}
